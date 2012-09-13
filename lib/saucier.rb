111673515fcf33304c716b4cb5054f2960e426ec
Shaun Dern <shaun@substantial.com>
2012-09-13 12:17:21 -0700

    fix symlink path

--- lib/saucier.rb
+++ lib/saucier.rb
@@ -38,8 +38,7 @@ module Capistrano::Saucier
 
         task :symlink_cookbooks do
           shared_cookbooks = File.join(shared_path, 'cookbooks')
-          current_cookbooks = File.join(current_release, 'cookbooks')
-          run "mkdir -p #{shared_cookbooks} && ln -s #{shared_cookbooks} #{current_cookbooks}"
+          run "mkdir -p #{shared_cookbooks} && ln -s #{shared_cookbooks} #{current_release}"
         end
 
         task :bundle_install do
